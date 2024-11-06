636a492c9bbf3d1761874c1b8bcea12573df0d36
Guilherme Moraes <127672759+guilhermemoraes1@users.noreply.github.com>
2024-11-06 14:11:04 -0300

    Update algoritmo2.js

--- src/algoritmo2.js
+++ src/algoritmo2.js
@@ -1,1 +1,28 @@
+// cria um arraylist chamado numbers
+const numbers = [1,2,3,4,5];
 
+// o código abaixo vai lançar um erro por não existir a lista chamada arr
+
+/*
+const output = arr.map((x)=>{
+  return x * 2;  
+});
+*/
+
+/*
+  A versão certa seria trocar o 'arr' pela lista 'numbers'
+
+  a função map itera sobre os elementos da lista numbers e retorna cada elemento multiplicado por 2,
+  assim atribui a variável output um array com todos os elementos de numbers multiplicado por 2
+*/ 
+
+const output = numbers.map((x)=>{
+    return x * 2;  
+});
+
+// mostra no console a lista numbers que não foi modificada
+
+// console.log(numbers);
+
+// caso queira mostrar a nova lista com os arrays modificados, deve usar a variável output
+console.log(output);
