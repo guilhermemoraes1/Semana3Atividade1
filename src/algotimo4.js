c8d5c141c286936036290fc43df13f692dfd2959
Guilherme Moraes <127672759+guilhermemoraes1@users.noreply.github.com>
2024-11-06 14:39:07 -0300

    Create algotimo4.js

---
+++ src/algotimo4.js
@@ -0,0 +1,19 @@
+// cria um arraylist chamado arr
+const arr = [1,2,3,4,5]
+
+// função filter itera os elementos de arr e retorna apenas aqueles que forem maiores que 4
+// atribuindo esse array com apenas um valor na variável output
+const output = arr.filter((x)=>  x > 4);
+
+// mostra no console o valor de output
+console.log(output);
+
+/*
+
+esse algoritmo cria uma lista com numeros,
+depois usa a função filter na lista arr, 
+assim iterando cada elemento e retornando apenas aqueles que forem maiores que 4
+e atribuindo essa array na variável output
+por útimo o console.log vai mostrar no console o valor da variável output.
+
+*/
